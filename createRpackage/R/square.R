183fb1b98d8f73ba95f0e8e3620594586f3b0c05
Minh-Tri Van <vanminhtri95@gmail.com>
2022-11-01 10:56:17 -0700

    Update

--- createRpackage/R/square.R
+++ createRpackage/R/square.R
@@ -1,3 +1,14 @@
+#' Square a number or vector of numbers
+#'
+#' @param x a number of a numeric vector to be squared
+#'
+#' @return the square of x
+#' @export
+#'
+#' @examples
+#' square(10) # should be 100
+#' square(c(2,4)) # should be c(4,16)
 square <-function(x) {
   x^2
 }
+
