3822a96ecde8d838875d0c2705a72dfec2823193
Minh-Tri Van <vanminhtri95@gmail.com>
2022-11-03 10:31:40 -0700

    Update

--- createRpackage/R/square.R
+++ createRpackage/R/square.R
@@ -1,6 +1,6 @@
-#' Square a number or vector of numbers
+#' Square a number or vector/list of numbers
 #'
-#' @param x a number of a numeric vector to be squared
+#' @param x a number of a numeric vector/list to be squared
 #'
 #' @return the square of x
 #' @export
@@ -8,7 +8,8 @@
 #' @examples
 #' square(10) # should be 100
 #' square(c(2,4)) # should be c(4,16)
+#' square(as.list(c(2,4))) # should be c(4,16)
 square <-function(x) {
-  x^2
+  purrr::map_dbl(x, function(y) y^2)
 }
 
