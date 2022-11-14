19d17855ee4e0e0e179d720c9dbe613f966c4eab
Minh-Tri Van <vanminhtri95@gmail.com>
2022-11-14 09:15:22 -0800

    complete

---
+++ createRpackage/NEWS.md
@@ -0,0 +1,9 @@
+# createRpackage 0.0.0.9000
+
+* Package can square
+
+# createRpackage 0.0.0.9000
+
+* Added a `NEWS.md` file to track changes to the package.
+
+
---
+++ createRpackage/R/patients.R
@@ -0,0 +1,15 @@
+#' World Health Organization TB data
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `patients`
+#' A data frame with 3 rows 2 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"patients"
